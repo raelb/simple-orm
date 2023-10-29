d12882d5169fc1a157f4bcc19a87875b5bac5079
Rael Bauer <rael.bauer@gmail.com>
2023-10-29 14:17:55 +0200

    remove core.log

--- src/Project02Server.dpr
+++ src/Project02Server.dpr
@@ -13,8 +13,8 @@ uses
   {$I mormot.uses.inc}
   SysUtils,
   mormot.core.base,
-  mormot.core.os,
-  mormot.core.log{,
+  mormot.core.os{,
+  mormot.core.log,
   mormot.db.raw.sqlite3,
   mormot.orm.core,
   mormot.rest.http.server  }
